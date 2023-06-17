a32418e4ea78b57179af6973f79e044ddcb9731a
Kishan Kumar <kishan.kumar@zimtive.com>
2023-06-17 14:54:57 +0530

    first push

---
+++ src/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div className='h-screen w-screen flex items-center justify-center'>
+        <div className='border border-black rounded-md h-[50%] w-[50%]'>
+            <div className=''></div>
+            <div className=''></div>
+            <div className=''></div>
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
---
+++ src/assets/Quotes.js
@@ -0,0 +1,10 @@
+const quotes = ["Spread love everywhere you go. Let no one ever come to you without leaving happier.",
+"It is during our darkest moments that we must focus to see the light.",
+"Do not go where the path may lead, go instead where there is no path and leave a trail.",
+"Don't judge each day by the harvest you reap but by the seeds that you plant.",
+"Be the change that you wish to see in the world.",
+"If you tell the truth, you don't have to remember anything.",
+"A friend is someone who knows all about you and still loves you",
+"The greatest glory in living lies not in never falling, but in rising every time we fall"];
+
+export default quotes;
